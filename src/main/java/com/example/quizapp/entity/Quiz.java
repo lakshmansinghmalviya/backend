fadb1ab93f517d889acb10997dc5d2e01b010d38
lakshman.malviya <lakshman.malviya@genesistechnologies.in>
2024-08-09 13:37:08 +0530

    Created the Quiz table and assigned the coluumn fields

--- src/main/java/com/example/quizapp/entity/Quiz.java
+++ src/main/java/com/example/quizapp/entity/Quiz.java
@@ -1,11 +1,14 @@
 package com.example.quizapp.entity;
 
 import java.time.LocalDateTime;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
@@ -14,47 +17,32 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "users")
+@Table(name = "quizzes")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class MyUser {
+public class Quiz {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long userId;
-
-	@Column(nullable = false, unique = true)
-	private String username;
-
-	@Column(name = "last_login")
-	private LocalDateTime lastLogin;
+	private Long id;
 
 	@Column(nullable = false)
-	private boolean logout;
+	private String title;
 
 	@Column(nullable = false)
-	private String name;
+	private String description;
 
 	@Column(nullable = false)
-	private String password;
-
-	@Column(name = "profile_pic")
-	private String profilePic;
-
-	@Column(nullable = false)
-	private String role;
-
-	@Column(nullable = true)
-	private String token;
+	private Long timelimit;
+	private Boolean randomizeQuestions;
+	private Long attemptedTimes;
+	private Boolean isActive;
 
 	@Column(name = "created_at", updatable = false)
 	private LocalDateTime createdAt;
 
 	@Column(name = "updated_at")
 	private LocalDateTime updatedAt;
-
-	private String bio;
-	private boolean isActive;
 
 	@PrePersist
 	protected void onCreate() {
