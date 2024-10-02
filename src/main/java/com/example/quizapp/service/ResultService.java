df331b1a40dc70ab439812f466faaa66095d8f3a
lakshman.malviya <lakshman.malviya@genesistechnologies.in>
2024-10-02 12:45:26 +0530

    Added removed the redundant code

--- src/main/java/com/example/quizapp/service/ResultService.java
+++ src/main/java/com/example/quizapp/service/ResultService.java
@@ -7,7 +7,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Service;
 
 import com.example.quizapp.dto.MessageResponse;
